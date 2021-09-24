d1e1e9e4edb088f5ea85f109ca6f7bf12e1b5176
Karluz Zilba <carlosmrsilva79@outlook.pt>
2021-09-25 00:30:50 +0100

    Initial

---
+++ Flyout1/Flyout1/Resources/teste.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyout1.Resources
+{
+    class teste
+    {
+    }
+}
--- Flyout1/Flyout1/ViewModels/FlyoutPageViewModel.cs
+++ Flyout1/Flyout1/ViewModels/FlyoutPageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿using Flyout1.Resources;
+using Flyout1.Views;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace Flyout1.ViewModels
@@ -10,7 +13,34 @@ namespace Flyout1.ViewModels
 
         }
 
-        public string MyTestString = "AnyString";
+        private string teste1 = "Coisa 1";
+        private string teste2 = "Coisa 2";
+        private string teste3 = "Coisa 3";
+
+        public List<FlyoutPageItem> flyoutPageItems
+        {
+            get
+            {
+                return new List<FlyoutPageItem>
+                    {
+                        new FlyoutPageItem {
+                            Title=teste1,
+                            IconSource="door_light.png",
+                            TargetType= typeof(DoorPage)
+                        },
+                        new FlyoutPageItem {
+                            Title=teste2,
+                            IconSource="company_light.png",
+                            TargetType= typeof(AboutPage)
+                        },
+                        new FlyoutPageItem {
+                            Title=teste3,
+                            IconSource="config_light.png",
+                            TargetType= typeof(ContactsPage)
+                        }
+                    };
+            }
+        }
 
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string name = "")
